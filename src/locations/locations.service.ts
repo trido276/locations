0a01621943551a6864dafd24606f5a80b63f1e6a
Tri Do <tridohuu96@gmail.com>
2024-04-22 00:41:54 +0700

    implement validate empty payload

--- src/locations/locations.service.ts
+++ src/locations/locations.service.ts
@@ -5,7 +5,6 @@ import UpdateLocationDto from './dto/updateLocation.dto'
 import { validate } from '../validate/validator'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
-import { log } from 'console'
 
 const HYPHEN_MINUS = "-"
 @Injectable()
@@ -28,7 +27,7 @@ export default class LocationsService {
       }
       )
       if (location) {
-        return location;
+        return location
       }
       throw new HttpException('Location not found', HttpStatus.NOT_FOUND)
     }
@@ -46,10 +45,10 @@ export default class LocationsService {
      * PARENT AND CHILD CAN NOT BE NULL AT THE SAME TIME
     */
 
+    validate(createLocationDto)
     let loc = new Location
     loc.parse_to_location(createLocationDto)
     Logger.log(`Creating Location with values: ${loc.toString()}`)
-    validate(loc)
     if (createLocationDto.parent) {
       const parentLocation = await this.getLocationById(createLocationDto.parent)
       loc.locationNumber = `${parentLocation.locationShortName} ${HYPHEN_MINUS} ${loc.locationShortName}`
@@ -57,39 +56,41 @@ export default class LocationsService {
       loc.locationNumber = loc.locationShortName
     }
 
-    const newLocation = this.locationsRepository.create(loc);
-    await this.locationsRepository.save(newLocation);
+    const newLocation = this.locationsRepository.create(loc)
+    await this.locationsRepository.save(newLocation)
     Logger.log(`Create successfully!`)
-    return newLocation;
+    return newLocation
   }
 
-  async updateLocation(id: string, location: UpdateLocationDto) {
+  async updateLocation(id: string, updateLocation: UpdateLocationDto) {
     /** 
      * if B parent A, C parent A, -> 1-1: C parent A, remove A child in B
      * 
      */
-    const loc = new Location
-    loc.parse_to_location(location)
-    Logger.log(`Update Location ${id} with values: ${loc.toString()}`)
-    validate(loc)
+    validate(updateLocation)
+    const location = new Location
+    location.parse_to_location(updateLocation)
+    Logger.log(`Update Location ${id} with values: ${location.toString()}`)
 
-    await this.locationsRepository.update(id, location);
+    // TODO: try catch
+    await this.locationsRepository.update(id, location)
 
     const updatedLocation = await this.getLocationById(id)
-    if (updatedLocation) {
-      return updatedLocation
+    if (!updatedLocation) {
+      throw new HttpException('Location not found', HttpStatus.NOT_FOUND)
     }
-    Logger.log(`Create successfully!`)
-    throw new HttpException('Location not found', HttpStatus.NOT_FOUND);
+
+    Logger.log(`Update successfully!`)
+    return updatedLocation
   }
 
   async deleteLocation(id: string) {
     /**
      * TODO: if remove the one have child: child must point to that one's parent and reupdate locationName
      */
-    const deleteResponse = await this.locationsRepository.delete(id);
+    const deleteResponse = await this.locationsRepository.delete(id)
     if (!deleteResponse.affected) {
-      throw new HttpException('Location not found', HttpStatus.NOT_FOUND);
+      throw new HttpException('Location not found', HttpStatus.NOT_FOUND)
     }
   }
 }
